ac8496a5ffc4c714d182b43a5f401b24daa4ebe6
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-09-30 13:26:20 -0400

    Got SA images in

--- ias.py
+++ ias.py
@@ -86,7 +86,7 @@ def index():
 
     pager_data = get_pager_data(page, num_pages)
     return render_template("template.html", 
-        title="Is Israel an apartheid state?",
+        title="Israel is an apartheid state!",
         page=page,
         images=images_for_this_page,
         pager=pager_data
