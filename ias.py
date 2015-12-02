ae1b0916fa9ab8c63fc5a310d73153e6cb21cb1d
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-12-02 17:06:09 -0500

    Added image

--- ias.py
+++ ias.py
@@ -45,7 +45,6 @@ def get_imagegroups():
                             '%B %d, %Y at %H:%M'
                     )
                     d[path] = {"date": date, "caption": caption}
-                    d["name"] = name
                     l.append(d)
         f.append(l)
     return f
